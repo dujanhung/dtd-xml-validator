96d5f3c173a96e733810471e77c09d9786d757b3
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-05-21 15:52:24 +0700

    Create script.py

---
+++ script.py
@@ -0,0 +1,25 @@
+from lxml import etree
+import sys
+class XML_Validator:
+ def __init__(self)->None:
+  self
+ def validate(self,path_dtd:str,path_xml:str)->bool:
+  self
+  try:
+   dtd=etree.DTD(path_dtd)
+   xsd=etree.parse(path_xml)
+   o=dtd.validate(xsd)
+   if dtd.error_log:
+    print(dtd.error_log)
+   return o
+  except Exception as e:
+   print(e)
+   return False
+def main()->int:
+ etree.XMLParser(load_dtd=True,dtd_validation=True,resolve_entities=True)
+ validator=XML_Validator()
+ a=validator.validate(sys.argv[1],sys.argv[2])
+ if not a:
+  return 1
+ return 0
+print(f"🏁 exit {main()}")
